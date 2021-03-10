5e2101a885089b0e87fe007b2c7dac69d1963c29
KaveeshBaddage <kbaddage98@gmail.com>
2021-03-10 19:23:06 +0530

    Create a view to list sheduled interviews

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -10,6 +10,7 @@ import NotFoundView from 'src/views/errors/NotFoundView';
 import ProductListView from 'src/views/product/ProductListView';
 import RegisterView from 'src/views/auth/RegisterView';
 import SettingsView from 'src/views/settings/SettingsView';
+import InterviewListView from 'src/views/interview/InterviewListView';
 
 const routes = [
   {
@@ -21,6 +22,7 @@ const routes = [
       { path: 'dashboard', element: <DashboardView /> },
       { path: 'products', element: <ProductListView /> },
       { path: 'settings', element: <SettingsView /> },
+      { path: 'interview', element: <InterviewListView /> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
   },
